ba8449a1fa97bfb3f29cb96d9e353eaa152c1894
tahseens <4791203+tahseens@users.noreply.github.com>
2025-07-06 15:44:11 +0200

    added run in task file

--- overandunderflow/underflow.cpp
+++ overandunderflow/underflow.cpp
@@ -2,8 +2,15 @@
 #include <limits>
 
 int main() {
+
     int minInt = std::numeric_limits<int>::min();
     std::cout << "Minimum int value: " << minInt << std::endl;
+
+   if (minInt == std::numeric_limits<int>::min()) {
+        std::cout<<"inside if condition"<<std::endl;
+        std::cout << "Underflow risk if subtracting 1!" << std::endl;
+        return 0;
+    }
 
     // Underflow happens here:
     int underflow = minInt - 1;
